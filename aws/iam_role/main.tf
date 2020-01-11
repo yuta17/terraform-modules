a721fc980c5ed992ca9d91fce0fd50a7214f5c16
Hassan <usgitan@gmail.com>
2020-01-11 17:13:03 +0900

    Use aws_iam_role_policy_attachment

--- aws/iam_role/main.tf
+++ aws/iam_role/main.tf
@@ -21,7 +21,7 @@ resource "aws_iam_policy" "main" {
   policy = var.policy
 }
 
-resource "aws_iam_policy_attachment" "main" {
+resource "aws_iam_role_policy_attachment" "main" {
   role       = aws_iam_role.main.name
   policy_arn = aws_iam_policy.main.arn
 }
