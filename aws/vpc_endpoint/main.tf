5976aa5c651aef390c3a07f48ef3e28ba84040d7
yuta17 <usgitan@gmail.com>
2020-06-13 10:52:51 +0900

    Fix count

--- aws/vpc_endpoint/main.tf
+++ aws/vpc_endpoint/main.tf
@@ -11,6 +11,8 @@ resource "aws_vpc_endpoint" "s3" {
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private_s3" {
+  count = length(var.private_route_table_ids)
+
   vpc_endpoint_id = aws_vpc_endpoint.s3.id
   route_table_id  = element(var.private_route_table_ids, count.index)
 }
