7a0a18d6e39f7b3b4a22c741c33014ce41bb7fc2
yuta17 <usgitan@gmail.com>
2020-06-17 21:55:15 +0900

    Add ec2 for ssm

---
+++ aws/ec2_for_ssm/main.tf
@@ -0,0 +1,76 @@
+data "aws_iam_policy" "ec2_for_ssm" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+data "aws_iam_policy_document" "ec2_for_ssm" {
+  source_json = data.aws_iam_policy.ec2_for_ssm.policy
+
+  statement {
+    effect = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:PutObject",
+      "logs:PutLogEvents",
+      "logs:CreateLogStream",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchGetImage",
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath",
+      "kms:Decrypt",
+    ]
+  }
+}
+
+resource "aws_iam_instance_profile" "ec2_for_ssm" {
+  name = "ec2-for-ssm"
+  role = module.ec2_for_ssm_role.iam_role_name
+}
+
+resource "aws_instance" "for_operation" {
+  ami = "ami-0c3fd0f5d33134a76"
+  instance_type = "t3.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_for_ssm.name
+  subnet_id = aws_subnet.private.*.id[0]
+  user_data = file("./user_data.sh")
+}
+
+resource "aws_s3_bucket" "operation" {
+  bucket = "operation-pragmatic-terraform"
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_group" "operation" {
+  name = "/operation"
+  retention_in_days = 180
+}
+
+resource "aws_ssm_document" "session_manager_run_shell" {
+  name = "SSM-SessionManagerRunShell"
+  document_type = "Session"
+  document_format = "JSON"
+
+  content = << EOF
+    {
+      "schemaVersion": "1.0",
+      "description": "Document to hold regional settings for Session Manager",
+      "sessionType": "Standard_Stream",
+      "inputs": {
+        "s3BucketName": "${aws_s3_bucket.operation.id}",
+        "cloudwatchLogGroupName": "${aws_cloudwatch_log_group.operation.name}"
+      }
+    }
+  EOF
+}
